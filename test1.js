e195fa56642e50122e399b7970646976b8e7c442
enamulhaque <enamulhaque028@gmail.com>
2020-07-03 19:43:13 +0600

    Uploaded files

---
+++ test1.js
@@ -0,0 +1,8 @@
+function changeText() {
+    var x = document.getElementById('change').value;
+    document.getElementById('ch1').innerHTML = x;
+    var x = document.getElementById('change').value;
+    document.getElementById('ch2').innerHTML = x;
+    var x = document.getElementById('change').value;
+    document.getElementById('ch3').innerHTML = x;
+}
